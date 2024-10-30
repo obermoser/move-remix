5a526c2808eedebfa697ef01623259aec35df39c
John Doe <bernhard.obermoser@icloud.com>
2024-10-30 19:03:58 +0100

    Initial

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,18 @@
 import {
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
 
+import "@fontsource-variable/inter/index.css";
 import "./tailwind.css";
-
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="de">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -32,6 +20,15 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <header>
+          <nav className="px-10 pt-5">
+            <Link className="text-2xl font-semibold" to="/" prefetch="intent">
+              <h1>
+                Move<span className="text-teal-500">DB</span>
+              </h1>
+            </Link>
+          </nav>
+        </header>
         {children}
         <ScrollRestoration />
         <Scripts />
